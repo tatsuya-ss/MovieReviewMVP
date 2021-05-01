0c64d8d05e999497972d23cb08bde95f3a06629a
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-05-01 10:22:24 +0900

    ReviewMovieViewControllerのレイアウト作成

---
+++ MovieReviewMVP/ReviewMovie/ReviewMoviePresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewMoviePresenter.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/04/30.
+//
+
+import Foundation
---
+++ MovieReviewMVP/ReviewMovie/ReviewMovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReviewMovieViewController.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/04/30.
+//
+
+import UIKit
+
+class ReviewMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
