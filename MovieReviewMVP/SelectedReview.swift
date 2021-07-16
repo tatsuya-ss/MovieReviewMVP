8763849138d9c2b07ab20080aba9a0347d2040fc
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-16 17:52:04 +0900

    Review用の管理クラスを作成

--- MovieReviewMVP/SelectedReview.swift
+++ MovieReviewMVP/SelectedReview.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+final class SelectedReview {
+    private var selectedReview: MovieReviewElement
+    
+    init(review: MovieReviewElement) {
+        selectedReview = review
+    }
+
+    func returnReview() -> MovieReviewElement {
+        print(#function, selectedReview)
+        return selectedReview
+    }
+
+    func checkIsChanged(reviewScore: Double, review: String) -> Bool {
+        var compareReview = review
+        if review == .placeholderString {
+            compareReview = ""
+        }
+        if selectedReview.reviewStars != reviewScore || selectedReview.review ?? "" != compareReview {  // 変更があるなら
+            return true
+        } else {  // 変更がないなら
+            return false
+        }
+    }
+
+    func update(isSavedAsReview: Bool? = nil,
+                saveDate: Date? = nil,
+                score: Double? = nil,
+                review: String? = nil) {
+        
+        if let isSavedAsReview = isSavedAsReview {
+            print(#function, isSavedAsReview)
+            selectedReview.isStoredAsReview = isSavedAsReview
+        }
+        
+        if let saveDate = saveDate {
+            print(#function, saveDate)
+            selectedReview.create_at = saveDate
+        }
+        
+        if let score = score {
+            print(#function, score)
+            selectedReview.reviewStars = score
+        }
+        
+        if let review = review {
+            print(#function, review)
+
+            if review.isEmpty || review == .placeholderString {
+                selectedReview.review = nil
+            } else {
+                selectedReview.review = review
+            }
+        }
+    }
+
+}
