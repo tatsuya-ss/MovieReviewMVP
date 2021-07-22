4a76101a567067d4a404f04d10664b0c65cd727d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-23 07:35:24 +0900

    TableViewCellを追加

---
+++ MovieReviewMVP/SettingManagement/Cell/SettingManagementTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingManagementTableViewCell.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/07/23.
+//
+
+import UIKit
+
+class SettingManagementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
