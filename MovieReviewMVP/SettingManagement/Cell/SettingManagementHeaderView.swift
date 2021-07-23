947914c037e294f60fe9c87e62ad128d3e24d1aa
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-23 17:33:17 +0900

    Headerのxibファイル作成

---
+++ MovieReviewMVP/SettingManagement/Cell/SettingManagementHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingManagementHeaderView.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/07/23.
+//
+
+import UIKit
+
+class SettingManagementHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
