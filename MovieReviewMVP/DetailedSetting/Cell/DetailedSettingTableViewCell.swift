b43577d4ddc9673c76b4e9dcc8b0ad4259c9bde5
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-26 10:22:29 +0900

    ユーザー詳細用のtableViewCellを作成

---
+++ MovieReviewMVP/DetailedSetting/Cell/DetailedSettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailedSettingTableViewCell.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/07/26.
+//
+
+import UIKit
+
+class DetailedSettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
