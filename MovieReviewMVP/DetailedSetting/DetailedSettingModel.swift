508e9595b4d9b36ba87e56e6dbecb20d70906337
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-07-26 09:39:19 +0900

    ユーザー詳細情報用のMVPを準備

---
+++ MovieReviewMVP/DetailedSetting/DetailedSettingModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailedSettingModel.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/07/26.
+//
+
+import Foundation
---
+++ MovieReviewMVP/DetailedSetting/DetailedSettingPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  DetailedSettingPresenter.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/07/26.
+//
+
+import Foundation
