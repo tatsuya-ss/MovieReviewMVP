b4521425065b274397d51bf3e7b0ecb7f68fb44b
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-08-12 07:49:13 +0900

    UserDefaultsを拡張

--- MovieReviewMVP/Extension/UserDefaults+Ext.swift
+++ MovieReviewMVP/Extension/UserDefaults+Ext.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension UserDefaults {
+    func saveNumberOfSaves() { // 保存回数を+1して保存
+        var numberOfSaves = self.loadNumberOfSaves()
+        numberOfSaves += 1
+        self.set(numberOfSaves, forKey: .numberOfSavesKey)
+    }
+    
+    func loadNumberOfSaves() -> Int { // 保存回数を読み込み
+        let numberOfSaves = self.integer(forKey: .numberOfSavesKey)
+        return numberOfSaves
+    }
+}
+
+private extension String {
+    static let numberOfSavesKey = "numberOfSaves"
+}
