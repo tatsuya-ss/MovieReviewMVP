ef0b70c7c7877b2666faf53344a08a64be9f71f6
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 17:13:18 +0900

    詳細取得のUseCase作成

--- MovieReviewMVP/Domain/UseCases/VideoWorkUseCase.swift
+++ MovieReviewMVP/Domain/UseCases/VideoWorkUseCase.swift
@@ -11,6 +11,8 @@ protocol VideoWorkUseCaseProtocol {
     func fetchVideoWorks(page: Int, query: String,
                          completion: @escaping ResultHandler<[MovieReviewElement]>)
     func fetchUpcomingVideoWorks(completion: @escaping ResultHandler<[MovieReviewElement]>)
+    func fetchVideoWorkDetail(videoWork: MovieReviewElement,
+                              completion: @escaping ResultHandler<[CastDetail]>)
 }
 
 final class VideoWorkUseCase: VideoWorkUseCaseProtocol {
@@ -25,6 +27,12 @@ final class VideoWorkUseCase: VideoWorkUseCaseProtocol {
         repository.fetchVideoWorks(page: page,
                                    query: query,
                                    completion: completion)
+    }
+    
+    func fetchVideoWorkDetail(videoWork: MovieReviewElement,
+                              completion: @escaping ResultHandler<[CastDetail]>) {
+        repository.fetchVideoWorkDetail(videoWork: videoWork,
+                                        completion: completion)
     }
     
     func fetchUpcomingVideoWorks(completion: @escaping ResultHandler<[MovieReviewElement]>) {
