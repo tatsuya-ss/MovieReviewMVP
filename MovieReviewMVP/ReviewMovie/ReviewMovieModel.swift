8dd5e18f6d4d2a678c49f505a1c8183549fb604d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 17:39:54 +0900

    モデルのキャスト情報取得処理削除

--- MovieReviewMVP/ReviewMovie/ReviewMovieModel.swift
+++ MovieReviewMVP/ReviewMovie/ReviewMovieModel.swift
@@ -11,7 +11,6 @@ protocol ReviewMovieModelInput {
     func checkSaved(movie: MovieReviewElement, completion: @escaping (Bool) -> Void)
     func reviewMovie(movieReviewState: MovieReviewStoreState, _ movie: MovieReviewElement)
     func fetchMovie(sortState: sortState, completion: @escaping (Result<[MovieReviewElement], Error>) -> Void)
-    func requestMovieDetail(completion: @escaping (Result<Credits, SearchError>) -> Void)
     func checkLoginState() -> Bool
 }
 
@@ -23,47 +22,6 @@ final class ReviewMovieModel : ReviewMovieModelInput {
     }
     
     let reviewUseCase = ReviewUseCase()
-    
-    func requestMovieDetail(completion: @escaping (Result<Credits, SearchError>) -> Void) {
-        guard let movie = movieReviewElement,
-              let encodingUrlString = TMDBDetailURL(id: movie.id, mediaType: movie.media_type ?? "movie").detailURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let encodeUrl = URL(string: encodingUrlString) else { return }
-        let urlRequest = URLRequest(url: encodeUrl)
-
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            do {
-                if let error = error {
-                    completion(.failure(.requestError(error)))
-                }
-                guard let data = data,
-                      let response = response as? HTTPURLResponse else {
-                    completion(.failure(.responseError))
-                    return
-                }
-                
-                if response.statusCode == 200 {
-                    let data: TMDBCredits = try JSONDecoder().decode(TMDBCredits.self, from: data)
-                    
-                    var castDetail: [CastDetail] = []
-                    for detail in data.cast {
-                        castDetail.append(CastDetail(cast: detail))
-                    }
-                    
-                    var crewDetail: [CrewDetail] = []
-                    for detail in data.crew {
-                        crewDetail.append(CrewDetail(crew: detail))
-                    }
-
-                    let credits: Credits = Credits(cast: castDetail, crew: crewDetail)
-                    completion(.success(credits))
-                }
-            } catch {
-                print(error)
-            }
-        }
-        task.resume()
-        
-    }
     
     func checkSaved(movie: MovieReviewElement, completion: @escaping (Bool) -> Void) {
         reviewUseCase.checkSaved(movie: movie) { result in
