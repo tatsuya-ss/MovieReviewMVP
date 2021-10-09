1bff53ba7a05c54052916a02aa600d4d6aee248f
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-09 16:42:40 +0900

    DataStoreの詳細取得処理作成

--- MovieReviewMVP/Domain/DataStores/TMDbDataStores/TMDbDataStore.swift
+++ MovieReviewMVP/Domain/DataStores/TMDbDataStores/TMDbDataStore.swift
@@ -19,6 +19,8 @@ protocol TMDbDataStoreProtocol {
     func fetchVideoWorks(page: Int, query: String,
                     completion: @escaping ResultHandler<TMDbSearchResponses>)
     func fetchUpcomingVideoWorks(completion: @escaping ResultHandler<TMDbSearchResponses>)
+    func fetchVideoWorkDetail(videoWork: VideoWork,
+                              completion: @escaping ResultHandler<TMDbCredits>)
 }
 
 final class TMDbDataStore: TMDbDataStoreProtocol {
@@ -57,6 +59,35 @@ final class TMDbDataStore: TMDbDataStoreProtocol {
         task.resume()
     }
     
+    func fetchVideoWorkDetail(videoWork: VideoWork,
+                              completion: @escaping ResultHandler<TMDbCredits>) {
+        guard let url = TMDbAPI.DetailsRequest(id: videoWork.id, mediaType: videoWork.mediaType ?? "movie").returnDetailsURLRequest() else {
+            completion(.failure(TMDbSearchError.urlError))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            do {
+                if let error = error {
+                    completion(.failure(TMDbSearchError.requestError))
+                    print(error)
+                }
+                guard let data = data,
+                      let response = response as? HTTPURLResponse else {
+                          completion(.failure(TMDbSearchError.responseError))
+                          return
+                      }
+                if response.statusCode == 200 {
+                    let data = try JSONDecoder().decode(TMDbCredits.self, from: data)
+                    completion(.success(data))
+                }
+            } catch {
+                completion(.failure(TMDbSearchError.responseError))
+                print(error)
+            }
+        }
+        task.resume()
+    }
+
     func fetchUpcomingVideoWorks(completion: @escaping ResultHandler<TMDbSearchResponses>) {
         guard let url = TMDbAPI.UpcomingRequest().upcomingURL
         else {
