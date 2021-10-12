a5dae954a487868e436986a25e69ac39a620f2fe
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-12 11:00:26 +0900

    User情報を扱うRepository作成

--- MovieReviewMVP/Domain/Repositories/UserRepository.swift
+++ MovieReviewMVP/Domain/Repositories/UserRepository.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol UserRepositoryProtocol {
+    func returnProfileInfomations() -> (String?, URL?)
+    func logout()
+    func returnCurrentUserEmail() -> String?
+    func returnloginStatus() -> Bool
+}
+
+final class UserRepository: UserRepositoryProtocol {
+    
+    private let dataStore: FirebaseDataStoreProtocol
+    init(dataStore: FirebaseDataStoreProtocol) {
+        self.dataStore = dataStore
+    }
+    
+    func returnProfileInfomations() -> (String?, URL?) {
+        dataStore.returnProfileInfomations()
+    }
+    
+    func logout() {
+        dataStore.logout()
+    }
+    
+    func returnCurrentUserEmail() -> String? {
+        dataStore.returnCurrentUserEmail()
+    }
+    
+    func returnloginStatus() -> Bool {
+        dataStore.returnloginStatus()
+    }
+    
+}
