7e683d0bdd45a9957078de35820b0b536a6ce56e
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-24 07:14:27 +0900

    SelectSearchReview画面を作成

---
+++ MovieReviewMVP/ReviewMovie/SelectSearchReview/Presenter/SelectSearchReview.swift
@@ -0,0 +1,8 @@
+//
+//  SelectSearchReview.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/12/24.
+//
+
+import Foundation
---
+++ MovieReviewMVP/ReviewMovie/SelectSearchReview/View/SelectSearchReviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SelectSearchReviewViewController.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/12/24.
+//
+
+import UIKit
+
+final class SelectSearchReviewViewController: UIViewController {
+    
+    private var reviewMovieOwner: ReviewMovieOwner!
+    
+    init() {
+        super.init(nibName: String(describing: Self.self), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        reviewMovieOwner = ReviewMovieOwner()
+        view.addSubview(reviewMovieOwner.reviewMovieView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        reviewMovieOwner.reviewMovieView.frame = view.frame
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        reviewMovieOwner.reviewTextResignFirstResponder()
+    }
+    
+}
