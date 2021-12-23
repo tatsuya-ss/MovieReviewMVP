8fe4668793dee45858ae0d9d630ee30bc696c220
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-24 06:30:21 +0900

    setupのエクステンションを最下部に移動

--- MovieReviewMVP/ReviewMovie/Views/ReviewMovieViewController.swift
+++ MovieReviewMVP/ReviewMovie/Views/ReviewMovieViewController.swift
@@ -123,120 +123,6 @@ extension ReviewMovieViewController {
     
 }
 
-// MARK: - setup
-private extension ReviewMovieViewController {
-
-    func setNavigationController() {
-        // MARK: ボタンの表示内容を分ける
-        switch presenter.returnMovieReviewState() {
-        case .beforeStore:
-            saveButton = UIBarButtonItem(title: .saveButtonTitle, style: .done, target: self, action: #selector(saveButtonTapped))
-            stopButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(stopButtonTapped))
-            stopButton.tintColor = .white
-            navigationItem.leftBarButtonItem = stopButton
-
-        case .afterStore(.reviewed):
-            saveButton = editButtonItem
-            saveButton.title = .editButtonTitle
-            stopButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(stopButtonTapped))
-            stopButton.tintColor = .white
-            navigationItem.leftBarButtonItem = stopButton
-
-        case .afterStore(.stock):
-            let backButton = UIBarButtonItem()
-            backButton.title = .stock
-            backButton.tintColor = .stringColor
-            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-            saveButton = UIBarButtonItem(title: .saveButtonTitle, style: .done, target: self, action: #selector(saveButtonTapped))
-        }
-        
-        saveButton.tintColor = .white
-        navigationItem.rightBarButtonItem = saveButton
-        navigationController?.navigationBar.barStyle = .black
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController?.navigationBar.shadowImage = UIImage()
-        
-        // MARK: NavigationBarのtitleに保存日を表示
-        guard let movieReviewElement = presenter.returnMovieReviewElement() else { return }
-        let saveDate = movieReviewElement.create_at
-        let movieReviewState = presenter.returnMovieReviewState()
-        let navigationTitle = DateFormat().convertDateToStringForNavigationTitle(date: saveDate, state: movieReviewState)
-        navigationItem.title = navigationTitle
-    }
-    
-    func setupTextView() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: nil, object: nil)
-        reviewMovieOwner.initReviewTextView()
-    }
-
-    private func setupBanner() {
-        let bannerSize = kGADAdSizeBanner
-        bannerView = GADBannerView(adSize: bannerSize)
-
-        addBannerViewToView(bannerView)
-
-        bannerView.delegate = self
-        
-        if let id = adUnitID(key: "banner") {
-            bannerView.adUnitID = id
-            bannerView.rootViewController = self
-            bannerView.load(GADRequest())
-            let adSize = GADAdSizeFromCGSize(CGSize(width: view.bounds.width, height: bannerSize.size.height))
-            bannerView.adSize = adSize
-        }
-        
-        func adUnitID(key: String) -> String? {
-            guard let adUnitIDs = Bundle.main.object(forInfoDictionaryKey: "AdUnitIDs") as? [String: String] else {
-                return nil
-            }
-            return adUnitIDs[key]
-        }
-    }
-    
-    private func addBannerViewToView(_ bannerView: GADBannerView) {
-        bannerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(bannerView)
-        bannerView.translatesAutoresizingMaskIntoConstraints = false
-        [bannerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-         bannerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-         bannerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)]
-            .forEach { $0.isActive = true }
-    }
-
-}
-// MARK: - @objc
-private extension ReviewMovieViewController {
-    
-    @objc func saveButtonTapped(_ sender: UIBarButtonItem) { // textViewに入力がない場合とある場合の保存処理
-        let reviewScore = reviewMovieOwner.returnReviewStarScore()
-        let review = reviewMovieOwner.returnReviewText()
-            presenter.didTapUpdateButton(editing: nil,
-                                         date: Date(),
-                                         reviewScore: reviewScore,
-                                         review: review)
-    }
-
-    
-    @objc func stopButtonTapped(_ sender: UIBarButtonItem) {
-            dismiss(animated: true, completion: nil)
-    }
-    
-    @objc private func keyboardWillShow(_ notification: Notification) {
-        guard let userInfo = notification.userInfo as? [String: Any],
-              let keyboardInfo = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-        
-        let keyboardSize = keyboardInfo.cgRectValue.size
-        let keyboardHeight = keyboardSize.height
-        reviewMovieOwner.keyboardHeight = keyboardHeight
-        
-        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight - view.safeAreaInsets.bottom, right: 0)
-        reviewMovieOwner.addContentInsets(insets: insets)
-
-    }
-
-}
-
-
 // MARK: - ReviewMoviePresenterOutput
 extension ReviewMovieViewController : ReviewMoviePresenterOutput {
     
@@ -342,6 +228,119 @@ extension ReviewMovieViewController : GADBannerViewDelegate {
 
     func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
       print("bannerViewDidDismissScreen")
+    }
+
+}
+
+// MARK: - setup
+private extension ReviewMovieViewController {
+
+    func setNavigationController() {
+        // MARK: ボタンの表示内容を分ける
+        switch presenter.returnMovieReviewState() {
+        case .beforeStore:
+            saveButton = UIBarButtonItem(title: .saveButtonTitle, style: .done, target: self, action: #selector(saveButtonTapped))
+            stopButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(stopButtonTapped))
+            stopButton.tintColor = .white
+            navigationItem.leftBarButtonItem = stopButton
+
+        case .afterStore(.reviewed):
+            saveButton = editButtonItem
+            saveButton.title = .editButtonTitle
+            stopButton = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(stopButtonTapped))
+            stopButton.tintColor = .white
+            navigationItem.leftBarButtonItem = stopButton
+
+        case .afterStore(.stock):
+            let backButton = UIBarButtonItem()
+            backButton.title = .stock
+            backButton.tintColor = .stringColor
+            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+            saveButton = UIBarButtonItem(title: .saveButtonTitle, style: .done, target: self, action: #selector(saveButtonTapped))
+        }
+        
+        saveButton.tintColor = .white
+        navigationItem.rightBarButtonItem = saveButton
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        // MARK: NavigationBarのtitleに保存日を表示
+        guard let movieReviewElement = presenter.returnMovieReviewElement() else { return }
+        let saveDate = movieReviewElement.create_at
+        let movieReviewState = presenter.returnMovieReviewState()
+        let navigationTitle = DateFormat().convertDateToStringForNavigationTitle(date: saveDate, state: movieReviewState)
+        navigationItem.title = navigationTitle
+    }
+    
+    func setupTextView() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: nil, object: nil)
+        reviewMovieOwner.initReviewTextView()
+    }
+
+    private func setupBanner() {
+        let bannerSize = GADAdSizeBanner
+        bannerView = GADBannerView(adSize: bannerSize)
+
+        addBannerViewToView(bannerView)
+
+        bannerView.delegate = self
+        
+        if let id = adUnitID(key: "banner") {
+            bannerView.adUnitID = id
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+            let adSize = GADAdSizeFromCGSize(CGSize(width: view.bounds.width, height: bannerSize.size.height))
+            bannerView.adSize = adSize
+        }
+        
+        func adUnitID(key: String) -> String? {
+            guard let adUnitIDs = Bundle.main.object(forInfoDictionaryKey: "AdUnitIDs") as? [String: String] else {
+                return nil
+            }
+            return adUnitIDs[key]
+        }
+    }
+    
+    private func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        [bannerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+         bannerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+         bannerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)]
+            .forEach { $0.isActive = true }
+    }
+
+}
+// MARK: - @objc
+private extension ReviewMovieViewController {
+    
+    @objc func saveButtonTapped(_ sender: UIBarButtonItem) { // textViewに入力がない場合とある場合の保存処理
+        let reviewScore = reviewMovieOwner.returnReviewStarScore()
+        let review = reviewMovieOwner.returnReviewText()
+            presenter.didTapUpdateButton(editing: nil,
+                                         date: Date(),
+                                         reviewScore: reviewScore,
+                                         review: review)
+    }
+
+    
+    @objc func stopButtonTapped(_ sender: UIBarButtonItem) {
+            dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo as? [String: Any],
+              let keyboardInfo = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        let keyboardHeight = keyboardSize.height
+        reviewMovieOwner.keyboardHeight = keyboardHeight
+        
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight - view.safeAreaInsets.bottom, right: 0)
+        reviewMovieOwner.addContentInsets(insets: insets)
+
     }
 
 }
