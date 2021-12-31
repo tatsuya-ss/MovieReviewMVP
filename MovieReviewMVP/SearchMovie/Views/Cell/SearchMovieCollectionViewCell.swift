9d82953192f4f349aae451ae2b23af0d93195619
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-31 16:02:59 +0900

    CollectionViewのCellファイル作成

---
+++ MovieReviewMVP/SearchMovie/Views/Cell/SearchMovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMovieCollectionViewCell.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2021/12/31.
+//
+
+import UIKit
+
+class SearchMovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
