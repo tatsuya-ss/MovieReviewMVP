8bbb5879ba4689d176e941d32c50244987a11b9d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-08 16:11:01 +0900

    検索結果用のセルを作成

---
+++ MovieReviewMVP/SearchMovie/Views/Cell/SearchResultCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  MovieReviewMVP
+//
+//  Created by 坂本龍哉 on 2022/01/08.
+//
+
+import UIKit
+
+final class SearchResultCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        movieImageView.layer.cornerRadius = movieImageView.bounds.height * 0.1
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        movieImageView.image = nil
+        titleLabel.text = nil
+    }
+    
+    func configure(image: UIImage?, title: String, releaseDay: String) {
+        movieImageView.image = image
+        titleLabel.text = title
+        releaseDateLabel.text = releaseDay
+    }
+    
+}
