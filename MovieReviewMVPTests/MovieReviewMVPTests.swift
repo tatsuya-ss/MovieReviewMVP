3b34167d710df3fa631e30b97391bc3f3cc83c93
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-11 18:18:42 +0900

    Recommendationsのテスト作成

--- MovieReviewMVPTests/MovieReviewMVPTests.swift
+++ MovieReviewMVPTests/MovieReviewMVPTests.swift
@@ -47,3 +47,75 @@ final class CachedSearchConditionsTests: XCTestCase {
     }
     
 }
+
+final class RecommendationsTests: XCTestCase {
+    
+    private var recommendations: Recommendations!
+    
+    override func setUp() {
+        recommendations = Recommendations()
+        super.setUp()
+    }
+    
+    func testおすすめ情報を配列に追加する() {
+        let fetchData = [VideoWork(id: 1), VideoWork(id: 2)]
+        
+        XCTContext.runActivity(named: "おすすめ映画情報を取得した時") { _ in
+            
+            XCTContext.runActivity(named: "公開中の映画") { _ in
+                recommendations.fetchNowPlaying(videoWorks: fetchData)
+                XCTAssertEqual(2, recommendations.nowPlaying.videoWorks.count)
+            }
+            
+            XCTContext.runActivity(named: "近日公開の映画") { _ in
+                recommendations.fetchUpcoming(videoWorks: fetchData)
+                XCTAssertEqual(2, recommendations.upcoming.videoWorks.count)
+            }
+
+            XCTContext.runActivity(named: "１週間のトレンドの映画") { _ in
+                recommendations.fetchTrendingWeek(videoWorks: fetchData)
+                XCTAssertEqual(2, recommendations.trendingWeek.videoWorks.count)
+            }
+            
+        }
+        
+    }
+    
+    func test作品のタイトル作成する() {
+        
+        XCTContext.runActivity(named: "タイトルがある場合") { _ in
+            recommendations.fetchNowPlaying(videoWorks: [VideoWork(title: "ナルト", id: 1)])
+            XCTAssertEqual("ナルト", recommendations.makeTitle(indexPath: IndexPath(item: 0, section: 0)))
+        }
+        
+        XCTContext.runActivity(named: "タイトルがなくオリジナルネームがある場合") { _ in
+            recommendations.fetchNowPlaying(videoWorks: [VideoWork(title: nil, originalName: "ナルト少年編", id: 1)])
+            XCTAssertNil(recommendations.nowPlaying.videoWorks[0].title)
+            XCTAssertEqual("ナルト少年編", recommendations.makeTitle(indexPath: IndexPath(item: 0, section: 0)))
+        }
+        
+        XCTContext.runActivity(named: "タイトルもオリジナルネームもない場合") { _ in
+            recommendations.fetchNowPlaying(videoWorks: [VideoWork(title: nil, originalName: nil, id: 1)])
+            XCTAssertNil(recommendations.nowPlaying.videoWorks[0].title)
+            XCTAssertNil(recommendations.nowPlaying.videoWorks[0].originalName)
+            XCTAssertEqual("タイトルがありません", recommendations.makeTitle(indexPath: IndexPath(item: 0, section: 0)))
+        }
+        
+    }
+    
+    func testリリース日を作成する() {
+        
+        XCTContext.runActivity(named: "リリース日がある場合") { _ in
+            recommendations.fetchNowPlaying(videoWorks: [VideoWork(releaseDay: "2020年11月11日", id: 1)])
+            XCTAssertEqual("(2020年11月11日)", recommendations.makeReleaseDay(indexPath: IndexPath(item: 0, section: 0)))
+        }
+        
+        XCTContext.runActivity(named: "リリース日がない場合") { _ in
+            recommendations.fetchNowPlaying(videoWorks: [VideoWork(id: 1)])
+            XCTAssertNil(recommendations.nowPlaying.videoWorks[0].releaseDay)
+            XCTAssertEqual("", recommendations.makeReleaseDay(indexPath: IndexPath(item: 0, section: 0)))
+        }
+        
+    }
+    
+}
