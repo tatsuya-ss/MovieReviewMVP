3500fe2f1f716d6001a97f6257efe58efeeb6bb6
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-12 18:07:23 +0900

    検索時のテストコード

--- MovieReviewMVPTests/SearchVideoWorksTests.swift
+++ MovieReviewMVPTests/SearchVideoWorksTests.swift
@@ -6,27 +6,127 @@
 //
 
 import XCTest
+@testable import MovieReviewMVP
 
-class SearchVideoWorksTests: XCTestCase {
+// MARK: - モックを返すスタブ
+final class VideoWorkUseCaseStub: VideoWorkUseCaseProtocol {
+    
+    var fetchVideoWorksResponses: Result<[VideoWork], Error>?
+    var fetchPosterImageResponses: Result<Data, Error>?
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // MARK: add
+    func addFetchVideoWorks(result: Result<[VideoWork], Error>) {
+        fetchVideoWorksResponses = result
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func addFetchPosterImage(result: Result<Data, Error>) {
+        fetchPosterImageResponses = result
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    // MARK: VideoWorkUseCaseProtocolのメソッド
+    func fetchVideoWorks(page: Int, query: String, completion: @escaping ResultHandler<[VideoWork]>) {
+        guard let responses = fetchVideoWorksResponses
+        else {
+            fatalError("fetchVideoWorksResponsesが見つかりません")
         }
+        completion(responses)
     }
+    
+    func fetchRecommendVideoWorks(completion: @escaping ResultHandler<[VideoWork]>) {
+        print(#function)
+    }
+    
+    func fetchTrendingWeekVideoWorks(completion: @escaping ResultHandler<[VideoWork]>) {
+        print(#function)
+    }
+    
+    func fetchNowPlayingVideoWorks(completion: @escaping ResultHandler<[VideoWork]>) {
+        print(#function)
+    }
+    
+    func fetchVideoWorkDetail(videoWork: VideoWork, completion: @escaping ResultHandler<[CastDetail]>) {
+        print(#function)
+    }
+    
+    func fetchPosterImage(posterPath: String?, completion: @escaping ResultHandler<Data>) {
+        guard let responses = fetchPosterImageResponses
+        else {
+            fatalError("fetchPosterImageResponsesが見つかりません")
+        }
+        completion(responses)
+    }
+    
+}
 
+// MARK: - スパイ
+final class SearchMoviePresenterOutputSpy: SearchMoviePresenterOutput {
+    
+    private(set) var countOfInvokingSearchInitial = 0
+    private(set) var countOfInvokingSearchRefresh = 0
+    private(set) var countOfInvokingReviewTheMovie = 0
+    private(set) var countOfInvokingDisplayStoreReviewController = 0
+    private(set) var countOfInvokingInitialRecommendation = 0
+
+    let reviewManagement = ReviewManagement()
+    
+    var searchInitialCalledWithVideoWorks: (() -> Void)?
+
+    func searchInitial() {
+        countOfInvokingSearchInitial += 1
+        searchInitialCalledWithVideoWorks?()
+    }
+    
+    func searchRefresh() {
+        print(#function)
+    }
+    
+    func reviewTheMovie(movie: VideoWork, movieUpdateState: MovieUpdateState) {
+        print(#function)
+    }
+    
+    func displayStoreReviewController() {
+        print(#function)
+    }
+    
+    func initialRecommendation() {
+        print(#function)
+    }
+    
+}
+
+// MARK: - XCTestCase
+final class SearchVideoWorks: XCTestCase {
+    
+    var spy: SearchMoviePresenterOutputSpy!
+    var stub: VideoWorkUseCaseStub!
+    var videoWorksMock: [VideoWork]!
+    
+    override func setUp() {
+        spy = SearchMoviePresenterOutputSpy()
+        stub = VideoWorkUseCaseStub()
+        videoWorksMock = VideoWork.mock()
+    }
+    
+    func test検索ボタンタップ時() {
+        
+        XCTContext.runActivity(named: "検索ワードがある時") { _ in
+            let presenter = SearchMoviePresenter(view: spy, useCase: stub)
+            stub.addFetchVideoWorks(result: .success(videoWorksMock))
+            stub.addFetchPosterImage(result: .success(Data()))
+            let exp = XCTestExpectation(description: "fetchMovie内で呼ばれるfetchVideoWorksの実行を待つ")
+            spy.searchInitialCalledWithVideoWorks = {
+                exp.fulfill()
+            }
+            presenter.fetchMovie(state: .search(.initial), text: "ナルト")
+            wait(for: [exp], timeout: 1)
+            XCTAssertEqual(2, presenter.getVideoWorks(section: 0).count)
+            XCTAssertTrue(spy.countOfInvokingSearchInitial == 1)
+        }
+        
+        XCTContext.runActivity(named: "検索ワードがない時") { _ in
+            print(#function)
+        }
+        
+    }
+    
 }
--- MovieReviewMVPTests/VideoWorksMock.swift
+++ MovieReviewMVPTests/VideoWorksMock.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+@testable import MovieReviewMVP
+
+// MARK: - モック作成
+extension VideoWork {
+    static func mock() -> [VideoWork] {
+        [
+            VideoWork(title: "ナルト",
+                      posterPath: nil,
+                      originalName: nil,
+                      backdropPath: nil,
+                      overview: nil,
+                      releaseDay: nil,
+                      reviewStars: 4.0,
+                      review: "かっこいい",
+                      createAt: nil,
+                      id: 100,
+                      isStoredAsReview: nil,
+                      mediaType: nil),
+            
+            VideoWork(title: "ナルト疾風伝",
+                      posterPath: nil,
+                      originalName: nil,
+                      backdropPath: nil,
+                      overview: nil,
+                      releaseDay: nil,
+                      reviewStars: 5.0,
+                      review: "👏",
+                      createAt: nil,
+                      id: 200,
+                      isStoredAsReview: nil,
+                      mediaType: nil)
+        ]
+    }
+}
